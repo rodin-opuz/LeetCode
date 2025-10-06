595d61134df897419f60370f61d93f396b9e39b3
Rodin Opuz <opuzrodin@gmail.com>
2025-10-06 11:25:38 +0100

    Delete Python/Easy directory

--- Python/Easy/0001-two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        checked = {}
-        for i, nums in enumerate(nums):
-            complement = target - nums
-            if complement in checked:
-                return [checked[complement], i]
-            checked[nums] = i
-        return []
